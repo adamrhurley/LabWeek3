d0c16a3ab34f94c17295444a1059b35342642226
adamrhurley <43179127+adamrhurley@users.noreply.github.com>
2018-10-08 22:16:01 +0100

    Exercise 5

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,2 +1,36 @@
 public class Bicycle {
-}
+
+    private String name;
+    private  double value;
+    private String make;
+
+    public Bicycle() {
+        this.name = "No Name";
+        this.value = 000.00;
+        this.make = "No Make";
+    }
+
+    public Bicycle(String name, double value, String make) {
+        setName(name);
+        setValue(value);
+        setMake(make);
+    }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setValue(double value) {
+            this.value = value;
+        }
+
+        public void setMake(String make) {
+            this.make = make;
+        }
+
+    @Override
+    public String toString() {
+        return "Owner's Name: " + name + "\nValue: " + value + "\nMake: " + make;
+    }
+    }
+
--- src/Bicycle1.java
+++ src/Bicycle1.java
@@ -1,2 +1,25 @@
+import javax.swing.*;
+
 public class Bicycle1 {
+        public static void main(String[] args) {
+            Bicycle bicycle = new Bicycle();
+
+            String name, make, valueAsString;
+            double value;
+
+            JOptionPane.showMessageDialog(null, "Bicycle Details: \n" + bicycle.toString());
+
+            name = JOptionPane.showInputDialog("Please enter your name:");
+
+            valueAsString = JOptionPane.showInputDialog("Please the value:");
+            value = Double.parseDouble(valueAsString);
+
+            make = JOptionPane.showInputDialog("Please enter the make:");
+
+            Bicycle bicycle1 = new Bicycle(name, value, make);
+
+
+            JOptionPane.showMessageDialog(null, "Bicycle Details: \n" + bicycle1.toString());
 }
+}
+
